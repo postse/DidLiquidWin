8918c6bcaf04674bce5a158ab432104b13447e00
Simon <simon.post.1999@gmail.com>
2020-11-08 12:52:13 -0500

    Transitions added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { styled } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import { text } from 'body-parser';
 
 const MyButton = styled(Button)({
   background: '#153A6B',
@@ -12,6 +13,8 @@ const MyButton = styled(Button)({
   color: 'white',
   height: 48,
   padding: '0 30px',
+  marginLeft: '5px',
+  marginRight: '5px',
   borderColor: '#ffffff',
   fontFamily: [
     '-apple-system',
@@ -39,11 +42,26 @@ class App extends Component {
 
   async componentDidMount(game) {
     if (game != null) {
-      axios.get('/data/' + game ).then((res) => {
+      let textToTransition = document.getElementsByClassName("transition");
+      for (let i = 0; i < textToTransition.length; i++) {
+        console.log(textToTransition[i]);
+        textToTransition[i].classList.remove("on");
+        textToTransition[i].classList.add("off");
+      }
+      
+      axios.get('/data/' + game).then((res) => {
         newstate = res.data.result[0]
         this.setState(newstate)
+      }).then(() => {
+        setTimeout(() => {
+          for (let i = 0; i < textToTransition.length; i++) {
+            console.log(textToTransition[i]);
+            textToTransition[i].classList.remove("off");
+            textToTransition[i].classList.add("on");
+          }
+        }, 100)
       })
-    } 
+    }
   }
 
   render() {
@@ -69,32 +87,32 @@ class App extends Component {
     }
 
     else {
-      status = "";
-      dash = "";
+      status = "PLACEHOLDER";
+      dash = "PLACEHOLDER";
     }
 
     return (
       <div className="App">
         <header className="App-header">
           <Toolbar>
-            <MyButton variant="contained" onClick={() => { this.componentDidMount("dota2"); game = "Dota 2";}}>Dota 2</MyButton>
+            <MyButton variant="contained" onClick={() => { this.componentDidMount("dota2"); game = "Dota 2"; }}>Dota 2</MyButton>
             <MyButton variant="contained" onClick={() => { this.componentDidMount("counterstrike"); game = "CS:GO"; }}>CS:GO</MyButton>
             {/* <MyButton variant="contained" onClick={() => { this.componentDidMount("pubg"); }}>PUBG</MyButton> */}
             {/* <MyButton variant="contained" onClick={() => { this.componentDidMount("starcraft2"); }}>SC2</MyButton> */}
             <MyButton variant="contained" onClick={() => { this.componentDidMount("rocketleague"); game = "Rocket League"; }}>Rocket League</MyButton>
-            <MyButton variant="contained" onClick={() => { this.componentDidMount("valorant"); game = "Valorant";}}>Valorant</MyButton>
-            <MyButton variant="contained" onClick={() => { this.componentDidMount("rainbowsix"); game = "R6S";}}>R6S</MyButton>
+            <MyButton variant="contained" onClick={() => { this.componentDidMount("valorant"); game = "Valorant"; }}>Valorant</MyButton>
+            <MyButton variant="contained" onClick={() => { this.componentDidMount("rainbowsix"); game = "R6S"; }}>R6S</MyButton>
             {/* <MyButton variant="contained" onClick={() => { this.componentDidMount("apexlegends"); }}>Apex Legends</MyButton> */}
             <MyButton variant="contained" onClick={() => { this.componentDidMount("leagueoflegends"); game = "LoL"; }}>LoL</MyButton>
           </Toolbar>
-          <p>
+          <p className='transition'>
             Did Team Liquid {game} Win?
           </p>
-          <p className = "won">
+          <p className="won transition off">
             {status}
           </p>
           <a
-            className="App-link"
+            className="App-link transition off"
             href={this.state.link}
             target="_blank"
             rel="noopener noreferrer"
